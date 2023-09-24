5b32d148f541067315feb4d7a94bafa3d39c1f14
Sindhura Mente <sindhuramente@gmail.com>
2023-09-23 21:17:47 -0400

    added some ui stuff

--- pages/organizer.js
+++ pages/organizer.js
@@ -5,14 +5,40 @@ import Head from 'next/head'
 
 function pendingApplicants() {
     return (
+        // <head>
+        //     <style>
+        //         {
+        //             box-sizing: border-box;
+        //         }
+        //         .column{
+        //         float: left;
+        //         width: 50%;
+        //         }
+
+        //         .row:after{
+        //         content: "";
+        //         display: table;
+        //         clear: both;
+        //         }
+        //     </style>
+        // </head>
         <section className="bg-green-100 min-h-screen">
+
             <title>Manage This Event</title>
             <div class="row">
-                <div class="column">Pending Participants</div>
-                <div class="column">Check-In Participants</div>
+                <div style="float:left">
+                    Pending Participants
+                </div>
+                <div style="float:right">
+                    Check-In Participants
+                </div>
             </div>
         </section >
     )
 }
 
 export default pendingApplicants;
+
+// const OTPInput = ({ id, value, onValueChange, handleSubmit }) => {
+
+// }
